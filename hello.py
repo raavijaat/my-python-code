f0cd5ca702bcf2cf749439e1dbab8a35e08916fb
raavijaat <146410287+raavijaat@users.noreply.github.com>
2023-09-29 11:45:03 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("hello world")
+print(100)
+print("there")
+print(10+23)
+a=100
+z="hey"
+print(z,a)
+# printing data type of vAARIABLE 
+print(type(a))
+#math operation
+u=10,v=5
+c=3,d=4
+p=u+v
+z=c*d
+print(p,z)
---
+++ task1.py
@@ -0,0 +1,7 @@
+num= int(input("enter the value of a"))
+add= int(input("enter the value of b"))
+
+sum=num+add
+print(sum)
+
+
